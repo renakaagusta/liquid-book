123dbf957550bb2cecc767713492cf90ce72707f
Bobby Fiando <bobfiando@gmail.com>
2024-12-14 12:26:36 +0700

    feat: in-progress bitmap

--- bitmap/src/lib.rs
+++ bitmap/src/lib.rs
@@ -9,28 +9,35 @@ use maths::tick_bitmap::TickBitmap;
 
 #[storage]
 #[entrypoint]
-pub struct OrderBook {
-    bitmap_storage: StorageMap<i16, StorageU256>,
+pub struct BitmapStorage {
+    storage: StorageMap<i16, StorageU256>,
 }
 
 #[public]
-impl OrderBook {
-    fn flip(&mut self, tick: i32) {
-        TickBitmap::flip_tick(&mut self.bitmap_storage, tick, 1);
+impl BitmapStorage {
+    pub fn position(&mut self, tick: i32) -> (i16, u8) {
+        let (word_pos, bit_pos) = TickBitmap::position(tick);
+        (word_pos, bit_pos)
     }
 
-    fn next_tick(&mut self, tick: i32, lte: bool) {
-        let (next, initialized) =
-            TickBitmap::next_initialized_tick_within_one_word(&self.bitmap_storage, tick, 1, lte);
-        console!("next: {}, initialized: {}", next, initialized);
+    pub fn flip(&mut self, tick: i32) -> (i16, u8) {
+        TickBitmap::flip_tick(&mut self.storage, tick, 1);
+        return self.position(tick);
     }
 
     fn get_bitmap(&mut self, index: i16) {
-        let bitmap = self.bitmap_storage.get(index);
+        let bitmap = self.storage.get(index);
         console!("{:b}", bitmap);
     }
 
-    fn test_bitmap(&mut self) {
+    pub fn next_tick(&mut self, tick: i32, lte: bool) -> (i32, bool) {
+        let (next, initialized) =
+            TickBitmap::next_initialized_tick_within_one_word(&self.storage, tick, 1, lte);
+        (next, initialized)
+    }
+
+    //TODO: remove test function
+    pub fn test_bitmap(&mut self) {
         assert_eq!(TickBitmap::position(0), (0, 0));
         assert_eq!(TickBitmap::position(256), (1, 0));
         assert_eq!(TickBitmap::position(257), (1, 1));
@@ -43,23 +50,23 @@ impl OrderBook {
 
         let mut masked = U256::from(1);
 
-        TickBitmap::flip_tick(&mut self.bitmap_storage, 0, 1);
-        assert_eq!(self.bitmap_storage.get(0), masked);
+        TickBitmap::flip_tick(&mut self.storage, 0, 1);
+        assert_eq!(self.storage.get(0), masked);
 
-        TickBitmap::flip_tick(&mut self.bitmap_storage, 253, 1);
+        TickBitmap::flip_tick(&mut self.storage, 253, 1);
         masked = masked | masked << 253;
-        assert_eq!(self.bitmap_storage.get(0), masked);
+        assert_eq!(self.storage.get(0), masked);
 
-        TickBitmap::flip_tick(&mut self.bitmap_storage, 254, 1);
+        TickBitmap::flip_tick(&mut self.storage, 254, 1);
         masked = masked | masked << 254;
-        assert_eq!(self.bitmap_storage.get(0), masked);
+        assert_eq!(self.storage.get(0), masked);
 
-        TickBitmap::flip_tick(&mut self.bitmap_storage, 255, 1);
+        TickBitmap::flip_tick(&mut self.storage, 255, 1);
         masked = masked | masked << 255;
-        assert_eq!(self.bitmap_storage.get(0), masked);
+        assert_eq!(self.storage.get(0), masked);
 
-        TickBitmap::flip_tick(&mut self.bitmap_storage, -1, 1);
-        assert_eq!(self.bitmap_storage.get(-1), U256::from(1) << 255);
+        TickBitmap::flip_tick(&mut self.storage, -1, 1);
+        assert_eq!(self.storage.get(-1), U256::from(1) << 255);
 
         console!("test flip passed");
     }
