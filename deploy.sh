0f5890ee1c9c5b33bb08294e0cda868f5a80df52
Bobby Fiando <bobfiando@gmail.com>
2024-12-18 21:35:59 +0700

    Update deploy and init script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# set -x
+
 private_key="0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659"
 
 # Define the modules
 modules=("bitmap" "engine" "order-manager" "tick-manager")
 
 # Define the deployment command
-deploy_command="cargo stylus deploy -e http://localhost:8547 --private-key \$private_key"
+deploy_command="cargo stylus deploy -e http://localhost:8547 --private-key \$private_key --no-verify"
 
 # Declare an associative array to store addresses
 declare -A addresses
@@ -14,15 +16,15 @@ declare -A addresses
 # Loop through each module and deploy
 for module in "${modules[@]}"; do
   echo "Deploying $module..."
-  output=$(cd $module && eval $deploy_command 2>&1)
+  output=$(cd "$(dirname "$0")/$module" && eval $deploy_command 2>&1)
   
   # Print the output for debugging
   # echo "Deployment output for $module:"
   # echo "$output"
   
-  # Parse the deployed contract address
-  address=$(echo "$output" | awk '/deployed code at address:/ {print $NF}')
-  
+  # Parse the deployed contract address and remove color codes
+  address=$(echo "$output" | sed -r 's/\x1b\[[0-9;]*m//g' | awk '/deployed code at address:/ {print $NF}')
+
   # Store the address in the associative array
   addresses[$module]=$address
   
@@ -38,8 +40,6 @@ done
 # Define the RPC URL
 rpc_url="http://localhost:8547"
 
-# Echo the cast commands
-echo "Run the following cast commands to initialize the contracts (Engine, Tick Manager, Order Manager):"
-echo "cast send --rpc-url \"$rpc_url\" --private-key \"$private_key\" \"${addresses[engine]}\" \"initialize(address,address,address)\" \"${addresses["tick-manager"]}\" \"${addresses[bitmap]}\" \"${addresses["order-manager"]}\""
-echo "cast send --rpc-url \"$rpc_url\" --private-key \"$private_key\" \"${addresses["tick-manager"]}\" \"initialize(address,address,address)\" \"${addresses[engine]}\" \"${addresses[bitmap]}\" \"${addresses["order-manager"]}\""
-echo "cast send --rpc-url \"$rpc_url\" --private-key \"$private_key\" \"${addresses["order-manager"]}\" \"initialize(address,address,address)\" \"${addresses[engine]}\" \"${addresses[bitmap]}\" \"${addresses["tick-manager"]}\""
+cast send "${addresses[engine]}" "initialize(address,address,address)" "${addresses["tick-manager"]}" "${addresses[bitmap]}" "${addresses["order-manager"]}" --rpc-url $rpc_url --private-key $private_key
+cast send "${addresses["tick-manager"]}" "initialize(address,address,address)" "${addresses[engine]}" "${addresses[bitmap]}" "${addresses["order-manager"]}" --rpc-url $rpc_url --private-key $private_key
+cast send "${addresses["order-manager"]}" "initialize(address,address,address)" "${addresses[engine]}" "${addresses[bitmap]}" "${addresses["tick-manager"]}" --rpc-url $rpc_url --private-key $private_key
